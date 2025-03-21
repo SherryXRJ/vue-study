5aff528795f6f1b8b4578b6df56cd7671977f23d
SherryXRJ <SherryXRJ@outlook.com>
2025-03-21 17:02:27 +0800

    study

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="person">
-      <h2>sum: {{sum}}</h2>
-      <button @click="add">sum</button>
-      <hr>
-      <img v-for="(dog, index) in dogList" :src="dog" :key="index">
-      <button @click="getDog">more dog</button>
-  </div>
+
 </template>
 
 <script lang="ts">
@@ -16,26 +10,9 @@
 
 <script lang="ts" setup>
  
-  import useSum from '@/hooks/useSum'
-  import useDog from '@/hooks/useDog'
- 
-  const {sum, add} = useSum()
-  const {dogList, getDog} = useDog()
-  
 
 </script>
 
 <style scoped>
-  .person {
-      background-color: yellowgreen;
-      padding: 20px;
-  }
 
-  button {
-      margin: 0 5px;
-  }
-
-  img {
-    height: 100px;
-  }
 </style>
---
+++ src/components/Person_hooks.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="person">
+      <h2>sum: {{sum}}</h2>
+      <button @click="add">sum</button>
+      <hr>
+      <img v-for="(dog, index) in dogList" :src="dog" :key="index">
+      <button @click="getDog">more dog</button>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+      name: 'Person'    //组件名
+  }
+</script>
+
+<script lang="ts" setup>
+ 
+  import useSum from '@/hooks/useSum'
+  import useDog from '@/hooks/useDog'
+ 
+  const {sum, add} = useSum()
+  const {dogList, getDog} = useDog()
+  
+
+</script>
+
+<style scoped>
+  .person {
+      background-color: yellowgreen;
+      padding: 20px;
+  }
+
+  button {
+      margin: 0 5px;
+  }
+
+  img {
+    height: 100px;
+  }
+</style>
